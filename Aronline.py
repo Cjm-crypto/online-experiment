af54c428a1c367d0906515bfa343031cf1fbe7f4
Cjm-crypto <cjmmmm@zjnu.edu.cn>
2026-05-06 18:10:52 +0800

    baocun

--- Aronline.py
+++ Aronline.py
@@ -165,6 +165,11 @@ elif current_stage in ["CDT_TASK", "CDT_FORMAL"]:
     placeholder.empty()
     st.markdown("#### 请判断探测面孔是否出现过？")
     st.image("https://via.placeholder.com/150", width=250) # 探测图
+
+    except Exception as e:
+        st.error(f"图片读取失败，请确保 GitHub 上有 '{folder}' 文件夹。错误信息: {e}")
+        if st.button("点击跳过此任务"):
+            next_stage()
     
     start_time = time.time()
     c1, c2 = st.columns(2)
